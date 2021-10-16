a3fd3d5401dc5a27019d19611e2d798a723349ec
echwaadams <adamsechwa18@gmail.com>
2021-10-16 08:22:18 +0300

    implemented profile service

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,21 @@ import { Injectable } from '@angular/core';
 })
 export class ProfileService {
 
-  constructor() { }
+  private username:string;
+  private client_id = '';
+  private client_secret = '';
+
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.client_id + "&client_secret=" + this.client_secret)
+  }
+  getProfileRepos(){
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id="+ this.client_id + "&client_secret=" + this.client_secret)
+  }
+
+  updateProfile(username:string){
+    this.username = username;
+  }
+  constructor(private http:HttpClient) { 
+    this.username = "echwaadams";
+  }
 }
