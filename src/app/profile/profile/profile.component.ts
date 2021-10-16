9d58457d016de9e99c995e08b7f4ace6c95cfb5d
echwaadams <adamsechwa18@gmail.com>
2021-10-16 08:34:58 +0300

    implemented observer to get user accounts

--- src/app/profile/profile/profile.component.ts
+++ src/app/profile/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  profile:any;
+  repos:any;
+  username!: string;
 
-  constructor() { }
+  constructor(private profileService:ProfileService) {
+    this.profileService.updateProfile(this.username);
+    this.profileService.getProfileInfo().subscribe((profile:any) => {
+      this.profile = profile;
+    });
+
+    this.profileService.getProfileRepos().subscribe(repos => {
+      this.repos = repos;
+    });
+   }
+   findProfile(){
+     this.profileService.updateProfile(this.username);
+     this.profileService.getProfileInfo().subscribe(profile => {
+       this.profile;
+     });
+
+     this.profileService.getProfileRepos().subscribe(repos => {
+       this.repos = repos;
+     })
+   }
 
   ngOnInit(): void {
+
+    this.profileService.updateProfile('echwaadams');
+    this.profileService.getProfileInfo().subscribe(profile => this.profile = profile);
+    this.profileService.getProfileRepos().subscribe(repos => this.repos = repos);
   }
 
 }
